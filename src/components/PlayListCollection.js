6c0580731397248870e855ac1e37f62ff514b6c4
nk2303 <ngankhong03@gmail.com>
2020-04-29 04:10:24 -0700

    in progress taking data from playlist

--- src/components/PlayListCollection.js
+++ src/components/PlayListCollection.js
@@ -3,11 +3,8 @@ import VideoList from './VideoList';
 
 const PlaylistCollection = (props) => {
 
-    
-
     return (
         <div className='row'>
-            
             <div className='col-3 white-translucent-02 margin-10px radius-5px'><h5 className='margin-10px'>Playlist name 1 </h5><br/><VideoList /></div>
             <div className='col-3 white-translucent-02 margin-10px radius-5px'><h5 className='margin-10px'>Playlist name 2 </h5><br/><VideoList /></div>
             <div className='col-3 white-translucent-02 margin-10px radius-5px'><h5 className='margin-10px'>Playlist name 3 </h5><br/><VideoList /></div>
@@ -15,4 +12,5 @@ const PlaylistCollection = (props) => {
     )
 }
 
-export default PlaylistCollection
+// export default PlaylistCollection 
+// export default connect(store => ({token: store.userContext.jwt}), mapDispatchToProps)(PlaylistCollection)
