2125c19f64300d6589d46324c3f3d2c931ea7775
nk2303 <ngankhong03@gmail.com>
2020-06-19 19:01:04 -0700

    deploy completed

--- src/actions/youtubeSearch.js
+++ src/actions/youtubeSearch.js
@@ -5,7 +5,7 @@ const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 export const searchYoutube = (q) => {
     return youtubeSearchApi(API_KEY, {q})
     .then(res => {
-        console.log(res);
+
         return {
             type: "SEARCH_COMPLETED",
             payload: res
