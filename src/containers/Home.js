9156fb133ad36323d25ccc7c98202d9ab2c1f07e
nk2303 <ngankhong03@gmail.com>
2020-06-23 21:37:32 -0700

    adding tutorial video to home

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,14 +2,18 @@ import VideoList from '../components/VideoList';
 import React from 'react';
 
 
-const Home = (props) => {
+const Home = () => {
 
     return (
         <div className="row home-pg black-bg h-800">
             <div className="col-3 max-height-800 overflow-auto"><VideoList /></div>
-            <div className="col-3 max-height-800 overflow-auto"></div>
-            <div className="col-3 max-height-800 overflow-auto"></div>
-            <div className="col-3 max-height-800 overflow-auto"></div>
+            <div className="col-9 max-height-800 pad-100">
+            <p>Youtube Playlist demo video</p>
+                <iframe 
+                    title='How to use Youtube Playlist'
+                    src='https://www.youtube.com/embed/axmKyyd579c'
+                    style={{'height': '600px', 'width': '1000px'}} />
+            </div>
         </div>
     )
 }
