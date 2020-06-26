e028629b52b81ebaf4cb3c1039efbd50049fbdfb
nk2303 <ngankhong03@gmail.com>
2020-06-26 15:32:50 -0700

    Load more videos from youtube api to 50

--- src/actions/youtubeSearch.js
+++ src/actions/youtubeSearch.js
@@ -3,7 +3,7 @@ import youtubeSearchApi from 'youtube-api-v3-search';
 
 const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 export const searchYoutube = (q) => {
-    return youtubeSearchApi(API_KEY, {q})
+    return youtubeSearchApi(API_KEY, {maxResults: 50, q})
     .then(res => {
 
         return {
--- src/components/LoadingSpinner.js
+++ src/components/LoadingSpinner.js
@@ -7,7 +7,7 @@ const LoadingSpinner = ({info}) => {
                 <span className="sr-only">Loading...</span>
             </div>
             <p className="text-info">Loading your {info}... </p>
-            <p>Please wait, this doesn't happen very often</p>
+            <p className="text-info">Please wait, this doesn't happen very often</p>
         </div>
     )
 }
