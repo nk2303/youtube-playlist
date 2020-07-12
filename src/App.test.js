9dcad4524b191e906a90a7f6fc6fa815a998c243
nk2303 <ngankhong03@gmail.com>
2020-07-11 22:51:44 -0700

    add test

--- src/App.test.js
+++ src/App.test.js
@@ -7,4 +7,5 @@ test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+  console.log('lol')
 });
